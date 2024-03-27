ab9c8f8ecedd2fb27244dd09ddbb248100ed4d5d
nikita <nkrasilnikov25@gmail.com>
2024-03-27 19:38:00 +0300

    Добвлен HTTP

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"text/template"
 )
 
+type Payload struct {
+	Count int
+}
+
 func main() {
+	http.HandleFunc("/", root)
 	http.HandleFunc("/assets/script.js", jFile)
 	http.HandleFunc("/index", index)
 	log.Println("Запущен сервер: http://127.0.0.1:8080")
@@ -38,5 +45,10 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func jFile(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "/Users/nikita/chat/assets/script.js")
+}
 
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(fmt.Sprintf("Color is %s", r.URL.Query().Get("color")))
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(Payload{Count: rand.Intn(5)})
 }
